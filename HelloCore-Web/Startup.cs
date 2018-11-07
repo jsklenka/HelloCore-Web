b8ca1fc5d882af0991884c09f8fae5a7aa72830f
jsklenka <jp.sklenka@pinnsg.com>
2018-11-07 15:03:28 -0500

    error to fail build

--- HelloCore-Web/Startup.cs
+++ HelloCore-Web/Startup.cs
@@ -16,7 +16,7 @@ namespace HelloCore_Web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            //Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
